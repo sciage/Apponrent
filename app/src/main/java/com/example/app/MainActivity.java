b9e82d62ce84ca8ceb8b641cd8843b3d8df077c1
sciage <dass.harish@gmail.com>
2020-10-26 15:21:23 +0530

    working mvvm lib todo

--- app/src/main/java/com/example/app/MainActivity.java
+++ app/src/main/java/com/example/app/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.android.architecture.blueprints.todoapp.tasks.TasksActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(MainActivity.this, TasksActivity.class);
+        startActivity(intent);
     }
 }
